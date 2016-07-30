29b6d53e3f1d3f89b9d2e656d2d761f765039693
Chen Wen <joc98@163.com>
2016-07-30 21:54:13 +0800

    change to ver 3.2.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     'summary': 'Simple and fancy notifications / alerts / errors for Meteor',
-    'version': '3.1.4',
+    'version': '3.2.0',
     'git': 'https://github.com/juliancwirko/meteor-s-alert.git',
     'name': 'juliancwirko:s-alert'
 });
